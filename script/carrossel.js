704168902cd011c42d5d64c00a85c26cca52339c
MatheusTeixeira1 <matheus2015.ts@gmail.com>
2025-04-25 15:36:36 -0300

    Codigo completo, index e pagina home

---
+++ script/carrossel.js
@@ -0,0 +1,10 @@
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+let currentSlide = 0;
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+setInterval(nextSlide, 3000);
